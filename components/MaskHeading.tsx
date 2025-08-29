1d095045ae0d7a1459b1298dc9ae7951218b5289
webwizzz <jatin.tomar@somaiya.edu>
2025-08-30 04:09:45 +0530

    Fix animation transition easing in MaskHeading component

--- components/MaskHeading.tsx
+++ components/MaskHeading.tsx
@@ -14,7 +14,7 @@ export function MaskHeading() {
     initial: { y: "100%" },
     enter: (i: number) => ({
       y: "0",
-      transition: { duration: 0.75, ease: [0.33, 1, 0.68, 1], delay: 0.075 * i }
+      transition: { duration: 0.75, ease: "easeInOut", delay: 0.075 * i }
     })
   };
 
